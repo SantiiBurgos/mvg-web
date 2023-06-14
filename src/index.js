16b92d0193b20f56f4e1ede9620b83cf65adbe8b
Santiago Jesus Burgos <burgossantiago@ibm.com>
2023-06-14 13:34:30 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/inicio"
+      <Route path="/"
        render={(props) => <Index {...props} />} />
       <Route
         path="/equipos"
@@ -42,7 +42,6 @@ root.render(
         path="/lol"
         render={(props) => <LOL {...props} />}
       />
-      <Redirect from="/" to="/inicio" />
     </Switch>
   </BrowserRouter>
 );
